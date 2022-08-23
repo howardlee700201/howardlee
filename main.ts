56ab3432ccfff5e561180c4b57550e189dcb0510
howardlee700201 <111855080+howardlee700201@users.noreply.github.com>
2022-08-23 15:57:21 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,15 @@
+basic.showNumber(5)
+basic.pause(1000)
+basic.showNumber(4)
+basic.pause(1000)
+basic.showNumber(3)
+basic.pause(1000)
+basic.showNumber(2)
+basic.pause(1000)
+basic.showNumber(1)
+basic.pause(1000)
+basic.showIcon(IconNames.Square)
+music.playMelody("G F G A - F E D ", 120)
+basic.forever(function () {
+	
+})
